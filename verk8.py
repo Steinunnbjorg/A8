08d9c23a6ec4835fdd8c3e35d2043c801c488b2b
patrekuratli <patrekur19@ru.is>
2020-09-17 16:02:15 +0000

    Update verk8.py

--- verk8.py
+++ verk8.py
@@ -1,6 +1,29 @@
-print('Hææ')
+x=1
+y=1   #byrjunarhnit
+while x!=3 and y!=1:
+    while x=1:
+        if y=1:
+            goN=True
+        if y=2:
+            goN=True
+            goE=Tr
+        # bara fara austur eða upp og niður 
+    #while y=1:
+        # má bara fara upp eða til hliðar
+    while x=2:
+        # allt mögulegt
+    #while y=2:
+        # allt mögulegt
+    while x=3:
+        # bara fara vestur eða upp og niður
+   # while y=3:
+        # má bara fara niður eða til hliðar
 
-print("bless")
-
-
-print('Hello world')
+    if inntak='n'
+        y+=1
+    if inntak='s'
+        y-=1
+    if inntak='e'
+        x+=1
+    if inntak='w'
+        x-=1
